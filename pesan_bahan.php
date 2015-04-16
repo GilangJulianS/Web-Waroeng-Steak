ead1a02dbe2f6c0eee2bc497d8f66eebb32a1084
Yanfa Adi Putra <yanfa.adiputra@gmail.com>
2015-04-16 21:32:52 +0700

    fungsionalitas pemesanan bahan udah

--- pesan_bahan.php
+++ pesan_bahan.php
@@ -5,22 +5,35 @@
  * Date: 16/04/2015
  * Time: 18:33
  */
+    $con = mysqli_connect("localhost","root","","tubes_si");
+    $counter_total = mysqli_real_escape_string($con, $_POST['counter_total']);
+    $username = mysqli_real_escape_string($con, $_POST['username']);
 
-    $post_id = uniqid();
-    $con=mysqli_connect("localhost","root","","wbd_db");
-    $tittle = mysqli_real_escape_string($con, $_POST['Judul']);
-    $post_date = mysqli_real_escape_string($con, $_POST['Tanggal']);
-    $konten = mysqli_real_escape_string($con, $_POST['Konten']);
-    if($tittle != null && $post_date != null && $konten != null){
-        $sql="INSERT INTO post (post_id, tittle, post_date, konten)
-        VALUES ('$post_id', '$tittle', '$post_date', '$konten')";
-        if (!mysqli_query($con,$sql)) {
-            die('Error: ' . mysqli_error($con));
+    //add query tabel pesanan
+    $date = "".date("Y-m-d");
+    $sql_pesanan = "INSERT INTO pesanan (tanggal, username_pemesan) VALUES ('$date', '$username')";
+    if (!mysqli_query($con, $sql_pesanan)) {
+        die('Error: ' . mysqli_error($con));
+    }
+    $result = mysqli_query($con,"SELECT * FROM pesanan WHERE id = (SELECT MAX(id) FROM pesanan)") or die(mysqli_error($con));
+    $row = mysqli_fetch_row($result);
+
+    $id_pesanan = $row[0];
+
+    for($i = 0; $i < $counter_total; $i++){
+        //add tabel bahan
+        $nama_bahan = mysqli_real_escape_string($con, $_POST['option_' . $i]);
+        $jumlah_bahan = mysqli_real_escape_string($con, $_POST['jumlah_' . $i]);
+        if ($nama_bahan != null && $jumlah_bahan != null) {
+            $sql_bahan = "INSERT INTO bahan (id_pesanan, nama_bahan, jumlah) VALUES ('$id_pesanan', '$nama_bahan', '$jumlah_bahan')";
+            if (!mysqli_query($con, $sql_bahan)) {
+                die('Error: ' . mysqli_error($con));
+            }
+        }
+        else{
+            echo "Judul, Tanggal, atau Konten tidak boleh kosong.";
         }
     }
     mysqli_close($con);
-    if($tittle == null || $post_date == null || $konten == null){
-        echo "Judul, Tanggal, atau Konten tidak boleh kosong.";
-    }
-    header('Location: index.php');
+    header('Location: index.html');
 ?>
